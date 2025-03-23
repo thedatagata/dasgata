ce864ac9c5d7200dcb28da770d2a115deb9f9909
Data Gata <thedatagata@gmail.com>
2025-03-23 17:40:01 -0400

    add_gtm

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -19,8 +19,18 @@ export default function App({ Component }: PageProps) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
         <link rel="stylesheet" href="/styles.css" />
         <script defer data-domain="dasgata.com" src="https://swamp-data-pipe.dasgata.com/js/script.hash.pageview-props.tagged-events.js"></script>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MNFD4V4T');`
+        }} />
+        {/* End Google Tag Manager */}      
       </Head>
       <div class="font-sans text-gray-900 bg-[#F8F6F0] min-h-screen flex flex-col">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNFD4V4T" height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
         <Component />
         <ScrollToTop />
       </div>
